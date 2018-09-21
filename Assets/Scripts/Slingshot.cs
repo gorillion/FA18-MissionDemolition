07e65bc9e95d306eeef0a0714b00c371ab9e24c8
David Farrell <gorillion@users.noreply.github.com>
2018-09-21 12:16:07 -0400

    Mission Demolition

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -81,6 +81,8 @@ public class Slingshot : MonoBehaviour {
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.shotFired();
+            ProjectileLine.S.poi = projectile;
         }
    }
 }
