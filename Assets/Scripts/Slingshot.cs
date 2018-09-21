58c90bdb3f61c945d0061cbaf7f209c1d9ba97c7
David Farrell <gorillion@users.noreply.github.com>
2018-09-21 08:05:03 -0400

    shooting basics

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
     [Header("Set In Inspector")]
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
 
     [Header("Set Dynamically")]
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+    private Rigidbody projectileRigidbody;
 
     void Awake()
     {
@@ -38,5 +40,33 @@ public class Slingshot : MonoBehaviour {
         projectile = Instantiate(prefabProjectile) as GameObject;
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
     }
+
+    void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+            Vector3 projPos = launchPos + mouseDelta;
+            projectile.transform.position = projPos;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+   }
 }
