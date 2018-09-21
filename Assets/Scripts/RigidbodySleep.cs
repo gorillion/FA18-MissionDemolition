7f3720cd7099c14e29927f923fa78005e423203f
David Farrell <gorillion@users.noreply.github.com>
2018-09-21 09:29:06 -0400

    castled

--- Assets/Scripts/RigidbodySleep.cs
+++ Assets/Scripts/RigidbodySleep.cs
@@ -6,11 +6,8 @@ public class RigidbodySleep : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb!= null) rb.Sleep();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
